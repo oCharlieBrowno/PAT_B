13028bc27fb939b43a0b6f5367416a7cf98e7e9f
oCharlieBrowno <1210139212@qq.com>
2017-01-02 23:37:35 +0800

    Add files via upload

---
+++ 1002写出这个数.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(void)
+{
+	char num[100];
+	for (int i = 0; i < 100; i++) {
+		printf("%d", num[i]);
+	}
+	
+	return 0;
+ } 
